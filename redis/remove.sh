e16bb6318dd0e8bf4ca2616269d0d2102dd3bc33
Krish Chowdhary <krish@redhat.com>
2020-06-16 13:47:05 -0400

    Modifes composition, renamed rediscluster

--- redis/remove.sh
+++ redis/remove.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 oc delete -f requirement-aws.yaml
 oc delete -f composition-aws-with-vpc.yaml
-oc delete crd redisclusterrequirements.cache.example.org
-oc delete crd redisclusters.cache.crossplane.io
-oc delete crd redisclusters.cache.example.org
+oc delete crd redisinstancerequirements.cache.example.org
+oc delete crd redisinstances.cache.crossplane.io
+oc delete crd redisinstances.cache.example.org
 oc delete -f publication.yaml
 oc delete -f definition.yaml

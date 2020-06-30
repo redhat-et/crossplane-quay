3aab86bd757a633272ae5ea53240f6bfb264db0b
Krish Chowdhary <krish@redhat.com>
2020-06-30 16:34:55 -0400

    slightly revised postgres deployment

--- postgres/setup.sh
+++ postgres/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 oc apply -f definition.yaml
+sleep 8
 oc apply -f publication.yaml
+sleep 8
 oc apply -f composition-aws-with-vpc.yaml
-sleep 10
+sleep 8
 oc apply -f requirement-aws.yaml

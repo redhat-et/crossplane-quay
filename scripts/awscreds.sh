b36f4ca992435220147b557c0b874afbc43f31dd
Krish Chowdhary <krish@redhat.com>
2020-09-08 13:11:37 -0400

    cleaned output

--- scripts/awscreds.sh
+++ scripts/awscreds.sh
@@ -1,5 +1,5 @@
 AWS_PROFILE=default && echo -e "[default]\naws_access_key_id = $(aws configure get aws_access_key_id --profile $AWS_PROFILE)\naws_secret_access_key = $(aws configure get aws_secret_access_key --profile $AWS_PROFILE)" > creds.conf
     
-oc create secret generic aws-creds -n crossplane-system --from-file=key=./creds.conf
+oc create secret generic aws-creds -n default --from-file=key=./creds.conf
 
 rm creds.conf

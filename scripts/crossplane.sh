69e2fc84aa6e211601f282b4bb7ea924ba941c7c
Krish Chowdhary <krish@redhat.com>
2021-03-02 16:25:08 -0500

    pin version for providers and crossplane

--- scripts/crossplane.sh
+++ scripts/crossplane.sh
@@ -7,4 +7,4 @@ oc create namespace crossplane-system
 helm repo add crossplane-stable https://charts.crossplane.io/stable
 helm repo update
 
-helm install crossplane --namespace crossplane-system crossplane-stable/crossplane --set securityContextCrossplane.runAsUser=null --set securityContextCrossplane.runAsGroup=null
+helm install crossplane --namespace crossplane-system crossplane-stable/crossplane --set securityContextCrossplane.runAsUser=null --set securityContextCrossplane.runAsGroup=null --version v1.0.0
